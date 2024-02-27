f17bf565e85f0a6092ec1dda8fac6745be404231
Maxim <maxim@redsquare.de>
2024-02-27 08:25:25 +0100

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+    Hi I this is me doing that in whatever time and place
+    """
+    st.write(content)
